ef2fc5d8bf294ae4a64df59b6a58b6efcc98a235
lete98 <joseluiscabezas@ceitartessos.com>
2019-10-21 16:41:25 +0200

    primera subida a github paleto

---
+++ Unity/Assets/SCRIPTS/HolaMundo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hola mundo");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
